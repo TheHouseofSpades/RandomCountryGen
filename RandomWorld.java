7f45092d203366fd8479a29d1a31c750a22218f3
TheHouseofSpades <f.p.l.freddie@gmail.com>
2015-02-15 00:34:24 -0600

    Added To RandomWorld

--- RandomWorld.java
+++ RandomWorld.java
@@ -1,18 +1,36 @@
 public class RandomWorld{
-  public double makeWealth(double w){
-    if(w > 0) return w;
-    int max = 16768100;
-    int min = 40;
+  public double makeWealth(String g, int s){
+     findGovNum(g);
+    int seed = 0;
+    seed = (int)(Math.random())*(s*(int)(Math.random()*2));
     double mean = 363448.69;
-    int posneg = (int)(Math.random());
-    if(posneg == 0){
-     return mean - (Math.random() * 36408.89); 
+    if(seed > 10){
+     return mean - (Math.random() * (5201.27*g)); 
     }
-    else if(posneg == 1){
-     return mean + (Math.random() * 1314651.31); 
+    else if(seed < 10){
+     return mean + (Math.random() * (187807.33*g)); 
     }
-    return 0;
-    
+  return 0;  
   }
-  
+  public int findGovNum(int g){
+   return 0; 
+  }
+  public int makePop(int g, int s){
+   findGovNum(g);
+   int max = 1368200000;
+   int min = 56;
+   int mean = 295344439;
+   seed = (int)(Math.random())*(s*(int)(Math.random()*2));
+     if(seed > 10){
+     return mean - (Math.random() * ((mean-min(/7))(*g))); 
+    }
+    else if(seed < 10){
+     return mean + (Math.random() * ((max-mean(/7))(*g))); 
+    }
+  return 0;
+  }
 }
+
+
+  
+  
--- RandomWorld.java~
+++ RandomWorld.java~
@@ -1,3 +1,18 @@
 public class RandomWorld{
+  public double makeWealth(double w){
+    if(w > 0) return w;
+    int max = 16768100;
+    int min = 40;
+    double mean = 363448.69;
+    int posneg = (int)(Math.random());
+    if(posneg == 0){
+     return mean - (Math.random() * 36408.89); 
+    }
+    else if(posneg == 1){
+     return mean + (Math.random() * 1314651.31); 
+    }
+    return 0;
+    
+  }
   
 }
